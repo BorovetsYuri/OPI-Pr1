14b0244990d2ba81b6cf3778a9ca0350c8890fc2
Yurii Borovets <sana22_byuo@student.ztu.edu.ua>
2023-09-20 23:27:06 +0300

    First task done

---
+++ OPI pr1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime;
+using System.Xml.Schema;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static double fraction(float a, float b)
+        {
+            float sum;
+            sum = a / b;
+            return Math.Pow(sum, 3);
+
+        }
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+
+            Console.WriteLine("Enter first number");
+            firstNumber =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(fraction(firstNumber, secondNumber));
+        }
+    }
+}
